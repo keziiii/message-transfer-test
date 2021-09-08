558cee0c203ff10dd67f36c5d93aba32a5ad24ef
Newp <mynewp@gmail.com>
2021-09-08 16:48:27 +0900

    파일분리

---
+++ EtronRtuServer/EtronRtuServer/RtuHeader.cs
@@ -0,0 +1,16 @@
+﻿public struct RtuHeader
+{
+    public byte[] Version { get; init; }
+    public byte CommandId { get; init; }
+    public byte[] Token { get; init; }
+    public UInt16 BodyLength { get; init; }
+    public byte[] TransactionId { get; init; }
+    public byte[] ModelCode { get; init; }
+    
+    public byte[] DeviceId { get; init; }
+    public string Imei { get; set; }
+    public byte DeviceIdLength { get; init; }
+    public byte EncryptionType { get; init; }
+    public byte StatusCode { get; init; }
+
+}
